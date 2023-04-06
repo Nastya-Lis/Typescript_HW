b8cd93e4f7fe51a75048088d626261f9b5a6c18b
Nastya-Lis <bankay.kotopes@mail.ru>
2023-04-06 17:01:20 +0300

    initial commit

---
+++ tps1.ts
@@ -0,0 +1,22 @@
+"use strict"
+
+//1
+let x = 23
+let y = 46
+let z = 12
+console.log(x+y+z)
+
+//2
+
+//2
+let seconds_in_minute = 60
+let minutes_in_hour = 60
+let hours_in_day = 24
+let days_in_year = 365
+
+function convertAge (age:number): number{
+    let x = age * 4
+    return x
+}
+
+convertAge(22)
